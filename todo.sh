707ce60788a4cd81931e710892f7decd481a44aa
utam0k <k0ma@utam0k.jp>
2019-10-03 19:59:01 +0900

    Change default order.

--- todo.sh
+++ todo.sh
@@ -6,20 +6,24 @@ readonly TODAY=$(date +"%m-%d")
 readonly WORKSPACE=$BASE/$YEAR
 
 function _new () {
-    LATEST=$(ls $WORKSPACE | head)
+    latest=$(ls $WORKSPACE | head)
     if [ ! -e $WORKSPACE ]; then
         mkdir -p $WORKSPACE
     fi
 
     if [ ! -e $WORKSPACE/$TODAY ]; then
         if [ ! -e $LATEST ]; then
-            cp $WORKSPACE/$LATEST $WORKSPACE/$TODAY
+            cat $WORKSPACE/$LATEST | grep -e "^- (" >> $WORKSPACE/$TODAY
         else
             touch $WORKSPACE/$TODAY
         fi
     else
         echo "Error! Already exit $TODAY."
     fi
+    return 0
+}
+
+function _add() {
     return 0
 }
 
@@ -30,9 +34,10 @@ function _open () {
 
 function _ls () {
     IFS=$'\n'
-    todos=$(grep $WORKSPACE/$TODAY -e "^- *")
+    todos=$(grep $WORKSPACE/$TODAY -e "^- *" | sort -k 2)
     if [ $P_FLG ]; then
         projects=$(grep $WORKSPACE/$TODAY -e "^- *" | awk '{ print $5 }' | sort | uniq )
+        # subprojects=$(grep $WORKSPACE/$TODAY -e "^- *" | awk '{ print $5 }' | sort | uniq )
         for pro in $projects; do
             echo "$pro"
             for t in $todos; do
@@ -54,6 +59,9 @@ shift
 case $subcommand in
     new)
         _new
+        ;;
+    add)
+        _add
         ;;
     open)
         _open
