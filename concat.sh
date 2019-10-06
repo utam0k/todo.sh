cfdd5c7c36cb65ee66e988c97874c922821508a7
utam0k <k0ma@utam0k.jp>
2019-10-06 11:10:20 +0900

    shellcheck

--- concat.sh
+++ concat.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FILES=("test/test.sh")
+readonly FILES=("test/test.sh")
 
 lines=$(grep -n source todo.sh | cut -d : -f 1 | xargs echo | sed -e 's/ /,/g')
 if [ "$lines" ]; then
     sed "$(( lines ))d" todo.sh > tmp.sh
 fi
 
-for file in $FILES; do
-    cat $file >> tmp.sh
+for file in "${FILES[@]}"; do
+    cat "$file" >> tmp.sh
 done
