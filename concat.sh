9e4fcf865116349d1443b91597d3d6260defb12c
utam0k <k0ma@utam0k.jp>
2019-10-06 12:23:58 +0900

    Deal with when there is no source command in todo.sh

--- concat.sh
+++ concat.sh
@@ -4,8 +4,9 @@ readonly FILES=("test/test.sh")
 lines=$(grep -n source todo.sh | cut -d : -f 1 | xargs echo | sed -e 's/ /,/g')
 if [ "$lines" ]; then
     sed "$(( lines ))d" todo.sh > tmp.sh
+    for file in "${FILES[@]}"; do
+        cat "$file" >> tmp.sh
+    done
+else
+    cp todo.sh tmp.sh
 fi
-
-for file in "${FILES[@]}"; do
-    cat "$file" >> tmp.sh
-done
