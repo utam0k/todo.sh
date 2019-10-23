fdabf2ce371c53470788f72a587a418c6507ceb8
utam0k <k0ma@utam0k.jp>
2019-10-23 19:20:43 +0900

    use the last todo instead of yesterday.

--- todo.sh
+++ todo.sh
@@ -11,22 +11,23 @@ readonly PROJECT_ROW=4
 source common.sh
 
 function _new () {
+    if [ -e "$TARGET_FILE" ]; then
+        echo "Error! Already exit $TARGET_FILE"
+        return 1
+    fi
+
+    latest_year=$(find "$BASE" -maxdepth 1 ! -wholename "$BASE" | sort -nr | head -n1)
+    latest_month=$(find "$latest_year" -maxdepth 1 ! -wholename "$latest_year" | sort -nr | head -n1)
+    latest_day=$(find "$latest_month" -maxdepth 1 ! -wholename "$latest_month" | sort -nr | head -n1)
+    prev_target="$latest_day/todo.txt"
+
     if [ ! -e "$TARGET_FOLDER" ]; then
         mkdir -p "$TARGET_FOLDER"
     fi
 
-    case "${OSTYPE}" in
-        freebsd*|darwin*) yesteday=$(date -v -1d "+%Y/%m/%d") ;;
-        linux*) yesteday=$(date --date '1 day ago' "+%Y/%m/%d")
-    esac
-    prev_target="$BASE/$yesteday/todo.txt"
-    if [ ! -e "$TARGET_FILE" ]; then
-        touch "$TARGET_FILE"
-        if [ -e "$prev_target" ]; then
-            grep -e "^- (" >> "$TARGET_FILE" < "$prev_target"
-        fi
-    else
-        echo "Error! Already exit $TARGET_FILE"
+    touch "$TARGET_FILE"
+    if [ -e "$prev_target" ]; then
+        grep -e "^- (" >> "$TARGET_FILE" < "$prev_target"
     fi
     return 0
 }
