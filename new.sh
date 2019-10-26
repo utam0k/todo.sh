968a6a513a0148311fb4fc52813b6903a2f66e03
utam0k <k0ma@utam0k.jp>
2019-10-27 00:42:47 +0900

    restore new.sh

--- new.sh
+++ new.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+function _new () {
+    if [ -e "$TARGET_FILE" ]; then
+        echo "Error! Already exit $TARGET_FILE"
+        return 1
+    fi
+
+    latest_year=$(find "$BASE" -maxdepth 1 ! -wholename "$BASE" | sort -nr | head -n1)
+    latest_month=$(find "$latest_year" -maxdepth 1 ! -wholename "$latest_year" | sort -nr | head -n1)
+    latest_day=$(find "$latest_month" -maxdepth 1 ! -wholename "$latest_month" | sort -nr | head -n1)
+    prev_target="$latest_day/todo.txt"
+
+    if [ ! -e "$TARGET_FOLDER" ]; then
+        mkdir -p "$TARGET_FOLDER"
+    fi
+
+    touch "$TARGET_FILE"
+    if [ -e "$prev_target" ]; then
+        grep -e "^- (" >> "$TARGET_FILE" < "$prev_target"
+    fi
+    return 0
+}
