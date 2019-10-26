ca54e359fea2771da817f7878de1de7aa7941e2f
utam0k <k0ma@utam0k.jp>
2019-10-26 23:16:16 +0900

    combine any source.

--- concat.sh
+++ concat.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
-# readonly FILES=("common.sh")
 
-lines=$(grep -n source todo.sh | cut -d : -f 1)
-if [ "$lines" ]; then
-    for line in $lines; do
-        # TODO
-        sed -e "$(( line ))d" -e "$(( line - 1 ))r common.sh" todo.sh > tmp.sh
-    done
-else
-    cp todo.sh tmp.sh
-fi
+aleready_sourced=()
+case "${OSTYPE}" in
+    freebsd*|darwin*) sed_i_option="-i """ ;;
+    linux*) sed_i_option="-i" ;;
+esac
+
+function replace () {
+    target_lines=()
+    while IFS='' read -r line; do target_lines+=("$line"); done < <(grep -n source tmp.sh)
+    if [ ${#target_lines[@]} -eq 0 ]; then
+        echo "Concat finished." >&2
+    else
+        line="${target_lines[0]}"
+        n=$(echo "$line" | cut -d : -f 1)
+        file=$(echo "$line" | cut -d : -f 2 | awk '{print $2}')
+        if [[ " ${aleready_sourced[@]} " =~ " ${file} " ]]; then
+            echo "[Warn] Already sourced $file." >&2
+            sed "$sed_i_option" -e "$(( n ))d" tmp.sh
+            replace
+            return 0
+        fi
+
+        if [ ! -e "$file" ]; then
+            echo "[Error!] Not found $file." >&2
+            return 1
+        fi
+        sed "$sed_i_option" -e "$(( n ))d" -e "$(( n - 1 ))r $file" tmp.sh
+        aleready_sourced+=("$file")
+        replace
+    fi
+}
+
+cp todo.sh tmp.sh
+replace
