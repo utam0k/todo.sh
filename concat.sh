0cccd0fcbd97dfe0bc23d34fba43b0be4ec18bf6
utam0k <k0ma@utam0k.jp>
2019-10-27 12:53:23 +0900

    fix typo.

--- concat.sh
+++ concat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aleready_sourced=()
+already_sourced=()
 
 shopt -s expand_aliases
 if sed --version 2>/dev/null | grep -q GNU; then
@@ -18,7 +18,7 @@ function replace () {
         line="${target_lines[0]}"
         n=$(echo "$line" | cut -d : -f 1)
         file=$(echo "$line" | cut -d : -f 2 | awk '{print $2}')
-        for already_file in "${aleready_sourced[@]}"; do
+        for already_file in "${already_sourced[@]}"; do
             if [[ "$already_file" = "$file" ]]; then
                 echo "[Warn] Already sourced $file." >&2
                 sedi -e "$(( n ))d" tmp.sh
@@ -32,7 +32,7 @@ function replace () {
             return 1
         fi
         sedi -e "$(( n ))d" -e "$(( n - 1 ))r $file" tmp.sh
-        aleready_sourced+=("$file")
+        already_sourced+=("$file")
         replace
     fi
 }
