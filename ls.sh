06b34808512b51ffed574e2692e2c1c2cf7ddbd9
utam0k <k0ma@utam0k.jp>
2019-11-06 23:01:11 +0900

    use buffer for performance.

--- ls.sh
+++ ls.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+function tab() {
+    printf "  "
+    return 0
+}
+
 function _ls () {
     check_is_exit_file "$TARGET_FILE"
 
     todos=()
+    output=""
     if [ "$A_FLG" ]; then
         # TODO: -ap
         while IFS='' read -r line; do todos+=("$line"); done < <(sort -k 2 < "$TARGET_FILE")
@@ -17,40 +23,41 @@ function _ls () {
         projects=()
         # shellcheck disable=SC2086
         while IFS='' read -r line; do projects+=("$line"); done < <(grep "$TARGET_FILE" -e "^- *" | awk '{ print $'${PROJECT_ROW}' }' | sort | uniq)
-
         for proj in "${projects[@]}"; do
             remainders=("${todos[@]:$pos}")
             if include_subproject "$proj" > /dev/null; then
                 parent=$(echo "$proj" | cut -d '+' -f 1)
                 sub=$(echo "$proj" | cut -d '+' -f 2)
                 if [ "$prevParent" == "" ]; then
-                    echo "$parent"
+                    output+="$parent\n"
                 elif [ "$prevParent" != "$parent" ]; then
-                    echo "$parent"
+                    output+="$parent\n"
                 fi
-                echo "  $sub"
+                output+="$(tab)$sub\n"
                 for t in "${remainders[@]}"; do
                     if echo "$t" | grep -e "^-.*$parent+$sub" > /dev/null; then
                         pos=$((pos + 1))
-                        echo "    $t"
+                        output+="$(tab)$(tab)$t\n"
                     fi
                 done
             else
-                echo "$proj"
+                output+="$proj\n"
                 for t in "${remainders[@]}"; do
                     # shellcheck disable=SC2086
                     if [ "$(echo "$t" | grep -e "^- *" | awk '{ print $'${PROJECT_ROW}' }')" == "$proj" ]; then
                         pos=$((pos + 1))
-                        echo "  $t"
+                        output+="$(tab)$t\n"
                     fi
                 done
             fi
             prevParent="$parent"
         done
+        printf "%b" "$output"
     else
         for (( i=0; i<${#todos[@]}; i++ )) do
-            echo "$i ${todos[$i]}"
+            output+="$i ${todos[$i]}\n"
         done
+        printf "%b" "$output"
     fi
     return 0
 }
