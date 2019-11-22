f77786f84d73950f28b1da917a54f93d00fe19b5
utam0k <k0ma@utam0k.jp>
2019-11-22 18:48:25 +0900

    fix bus.

--- diff.sh
+++ diff.sh
@@ -33,19 +33,13 @@ function _diff() {
     prev_target="$prev_day/todo.txt"
     check_is_exit_file "$prev_target"
 
-    todos=()
-    prev_todos=()
+    local todos=() prev_todos=()
     while IFS='' read -r line; do todos+=("$line"); done < <(sort -k 2 < "$TARGET_FILE")
     while IFS='' read -r line; do prev_todos+=("$line"); done < <(sort -k 2 < "$prev_target")
 
-    diff=()
-    for prev_t in "${prev_todos[@]}"; do
-        skip=
-        for t in "${todos[@]}"; do
-            [[ "$prev_t" == "$t" ]] && { skip=1; break; }
-        done
-        [[ -n $skip ]] || diff+=("$t")
-    done
+    local intersection=() diff=()
+    while IFS='' read -r line; do intersection+=("$line"); done < <(for item in "${todos[@]}" "${prev_todos[@]}"; do echo "$item"; done | sort | uniq -d)
+    while IFS='' read -r line; do diff+=("$line"); done < <(for item in "${todos[@]}" "${intersection[@]}"; do echo "$item"; done | sort | uniq -u)
 
     for t in "${diff[@]}"; do printf "%s\n" "$t"; done
 
