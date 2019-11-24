3dc054c53a751585b00b34cf13c567abd283018d
utam0k <k0ma@utam0k.jp>
2019-11-24 21:37:15 +0900

    easily compare diffs.

--- diff.sh
+++ diff.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
 
 function _diff() {
-    check_is_exit_file "$TARGET_FILE"
-
-    while getopts "y:m:d:" opts
-    do
-        case $opts in
-            y) require_year=$OPTARG ;;
-            m) require_month=$OPTARG ;;
-            d) require_day=$OPTARG ;;
-            *) echo "usage: diff [-y year] [-m month] [-d day]"
-               exit 1 ;;
-        esac
-    done
-    shift $((OPTIND - 1))
-
-    if [ "$require_year" ]; then
-        prev_year="$BASE_FOLDER/$require_year"
+    local end_target start_date
+    if [ "$#" -eq 1 ]; then
+        start_date=$(date -j -f "%Y-%m-%d" "$1" "+%Y/%m/%d")
+        start_target="$BASE_FOLDER/$start_date/todo.txt"
+        end_target="$TARGET_FILE"
+    elif [ "$#" -eq 2 ]; then
+        start_date=$(date -j -f "%Y-%m-%d" "$1" "+%Y/%m/%d")
+        end_date=$(date -j -f "%Y-%m-%d" "$2" "+%Y/%m/%d")
+        start_target="$BASE_FOLDER/$start_date/todo.txt"
+        end_target="$BASE_FOLDER/$end_date/todo.txt"
     else
-        prev_year=$(find "$BASE_FOLDER" -maxdepth 1 ! -wholename "$BASE_FOLDER" | sort -nr | head -n1 | tail -n1)
+        start_year=$(find "$BASE_FOLDER" -maxdepth 1 ! -wholename "$BASE_FOLDER" | sort -nr | head -n1 | tail -n1)
+        start_month=$(find "$start_year" -maxdepth 1 ! -wholename "$start_year" | sort -nr | head -n1 | tail -n1)
+        start_day=$(find "$start_month" -maxdepth 1 ! -wholename "$start_month" | sort -nr | head -n2 | tail -n1)
+        start_date=${start_day#$BASE_FOLDER/}
+        end_date=$(date +'%Y/%m/%d')
+        start_target="$start_day/todo.txt"
+        end_target=$TARGET_FILE
     fi
-    if [ "$require_month" ]; then
-        prev_month="$prev_year/$require_month"
-    else
-        prev_month=$(find "$prev_year" -maxdepth 1 ! -wholename "$prev_year" | sort -nr | head -n1 | tail -n1)
-    fi
-    if [ "$require_day" ]; then
-        prev_day="$prev_month/$require_day"
-    else
-        prev_day=$(find "$prev_month" -maxdepth 1 ! -wholename "$prev_month" | sort -nr | head -n2 | tail -n1)
-    fi
-    prev_target="$prev_day/todo.txt"
-    check_is_exit_file "$prev_target"
+    check_is_exit_file "$start_target"
+    check_is_exit_file "$end_target"
+    printf "%s...%s\n" "$start_date" "$end_date" >&2
 
-    local todos=() prev_todos=()
-    while IFS='' read -r line; do todos+=("$line"); done < <(sort -k 2 < "$TARGET_FILE")
-    while IFS='' read -r line; do prev_todos+=("$line"); done < <(sort -k 2 < "$prev_target")
+    local todos=() start_todos=()
+    while IFS='' read -r line; do todos+=("$line"); done < <(sort -k 2 < "$end_target")
+    while IFS='' read -r line; do start_todos+=("$line"); done < <(sort -k 2 < "$start_target")
 
     local intersection=() diff=()
-    while IFS='' read -r line; do intersection+=("$line"); done < <(for item in "${todos[@]}" "${prev_todos[@]}"; do echo "$item"; done | sort | uniq -d)
+    while IFS='' read -r line; do intersection+=("$line"); done < <(for item in "${todos[@]}" "${start_todos[@]}"; do echo "$item"; done | sort | uniq -d)
     while IFS='' read -r line; do diff+=("$line"); done < <(for item in "${todos[@]}" "${intersection[@]}"; do echo "$item"; done | sort | uniq -u)
 
     for t in "${diff[@]}"; do printf "%s\n" "$t"; done
