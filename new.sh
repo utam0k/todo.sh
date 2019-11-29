d97743fd1c43ddd323edf2badbce5f07dcddeda2
utam0k <k0ma@utam0k.jp>
2019-11-29 12:16:41 +0900

    fix bug.

--- new.sh
+++ new.sh
@@ -6,13 +6,15 @@ function _new () {
         return 1
     fi
 
-    if [ ! -e "$TARGET_FOLDER" ]; then
-        mkdir -p "$TARGET_FOLDER"
+    if [ ! -e "$BASE_FOLDER" ]; then
+        # TODO: Firt init
+        mkdir -p "$BASE_FOLDER"
     else
         latest_year=$(find "$BASE_FOLDER" -maxdepth 1 ! -wholename "$BASE_FOLDER" | sort -nr | head -n1)
         latest_month=$(find "$latest_year" -maxdepth 1 ! -wholename "$latest_year" | sort -nr | head -n1)
         latest_day=$(find "$latest_month" -maxdepth 1 ! -wholename "$latest_month" | sort -nr | head -n1)
         prev_target="$latest_day/todo.txt"
+        mkdir -p "$TARGET_FOLDER"
     fi
 
     touch "$TARGET_FILE"
